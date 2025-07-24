93d6d15e5c2c5a574d20bd8da70b67cbb1dd3d5e
sasi17reka <srsasi19@gmail.com>
2025-07-24 16:27:30 +0530

    Day2task

---
+++ DataType.java
@@ -0,0 +1,25 @@
+package day2;
+
+
+
+public class DataType {
+	public static void main(String[] args) {
+	int a=5;
+	float b=5.5f;
+	boolean c=true;
+	byte d=126;
+	short e=14;
+	long f=3263534434l;
+	double g=454.9879;
+	char h='a';
+	System.out.println("The integer:"+a);
+	System.out.println("The float:"+b);
+	System.out.println("The boolean:"+c);
+	System.out.println("The byte:"+d);
+	System.out.println("The short:"+e);
+	System.out.println("The long:"+f);
+	System.out.println("The double:"+g);
+	System.out.println("The character:"+h);
+	
+	}
+}
