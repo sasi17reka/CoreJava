3a8598eb03d7ac844a91834aae78b3403a68d56e
sasi17reka <srsasi19@gmail.com>
2025-07-25 16:14:24 +0530

    day2

---
+++ Stringstory.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Stringstory {
+
+		public static void main(String[] args) {
+			String name1="Tara";
+			String adjective1="beautiful";
+			String adjective2="Peaceful";
+			String noun1="book";
+			String noun2="chair";
+			String verb1="sketch";
+			String story="As the sunset over the horizon "+name1+" walked along the beach where the warm sand felt soothing beneath her feet and then she stumbled upon a "+adjective1+" shell which she picked up and held close "+adjective2+" Peaceful waves crashed against the shore creating a calming melody She sat in the "+noun1+" and started reading the "+noun2+" She began to "+verb1+" the scenery and finished with a lovely drawing";
+		    System.out.println(story);
+		
+		}
+		
+
+	}
+
